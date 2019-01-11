d7d3bee65564d7e0aaff339a0a2d0900bf36305c
Janardhana C R <janardhan@digiconnectt.com>
2019-01-11 12:58:55 +0530

    Restrict back button from login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -78,5 +78,10 @@
             </div> <!-- end row -->
         </div> <!-- end container -->
     </form>
-
+<script>
+    history.pushState(null, null, location.href);
+    window.onpopstate = function () {
+        history.go(1);
+    };
+</script>
 @stop
