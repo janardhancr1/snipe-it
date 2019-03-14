c0ae7526e16bb1fe2a2057af18513660bc005149
Janardhana C R <janardhan@digiconnectt.com>
2019-03-14 13:40:05 +0530

    Fix custom export issue

--- resources/views/partials/forms/edit/category-select.blade.php
+++ resources/views/partials/forms/edit/category-select.blade.php
@@ -4,7 +4,7 @@
     {{ Form::label($fieldname, $translated_name, array('class' => 'col-md-3 control-label')) }}
 
     <div class="col-md-7{{  ((isset($required)) && ($required=='true')) ? ' required' : '' }}">
-        <select class="js-data-ajax" {{ $assets_count > 0 ? 'disabled' : ''}} data-endpoint="categories/{{ (isset($category_type)) ? $category_type : 'assets' }}" data-placeholder="{{ trans('general.select_category') }}" name="{{ $fieldname }}" style="width: 100%" id="category_select_id">
+        <select class="js-data-ajax" {{ isset($assets_count) && category_type > 0 ? 'disabled' : ''}} data-endpoint="categories/{{ (isset($category_type)) ? $category_type : 'assets' }}" data-placeholder="{{ trans('general.select_category') }}" name="{{ $fieldname }}" style="width: 100%" id="category_select_id">
             @if ($category_id = Input::old($fieldname, (isset($item)) ? $item->{$fieldname} : ''))
                 <option value="{{ $category_id }}" selected="selected">
                     {{ (\App\Models\Category::find($category_id)) ? \App\Models\Category::find($category_id)->name : '' }}
@@ -18,7 +18,7 @@
     <div class="col-md-1 col-sm-1 text-left">
         @can('create', \App\Models\Category::class)
             @if ((!isset($hide_new)) || ($hide_new!='true'))
-                <a style="{{ $assets_count > 0 ? 'display:none' : ''}}" href='{{ route('modal.category',['category_type' => isset($category_type) ? $category_type : 'assets' ]) }}' data-toggle="modal"  data-target="#createModal" data-select='category_select_id' class="btn btn-sm btn-default">New</a>
+                <a style="{{ isset($assets_count) && category_type > 0 ? 'display:none' : ''}}" href='{{ route('modal.category',['category_type' => isset($category_type) ? $category_type : 'assets' ]) }}' data-toggle="modal"  data-target="#createModal" data-select='category_select_id' class="btn btn-sm btn-default">New</a>
             @endif
         @endcan
     </div>
